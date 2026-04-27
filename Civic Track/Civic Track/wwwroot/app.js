66ec77b25edab04c48ddcaa5474a0e29d9600516
Nduwayezu Sophonie <ntarerugamba1962@gmail.com>
2026-04-27 11:26:05 +0200

    updated frontend to talk to cloud than local api

--- Civic Track/Civic Track/wwwroot/app.js
+++ Civic Track/Civic Track/wwwroot/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://online-complaint-and-investigation.onrender.com'; 
+const API_BASE_URL = 'https://online-complaint-and-investigation.onrender.com/api'; 
 let currentAdminCaseId = null; 
 let _geoWatchId = null;
 
