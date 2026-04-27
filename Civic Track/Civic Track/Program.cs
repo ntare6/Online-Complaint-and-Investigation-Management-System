42c25db7fce497d64116b9f0280a2d75223bf1dd
Nduwayezu Sophonie <ntarerugamba1962@gmail.com>
2026-04-27 16:36:04 +0200

    Add vercel to talk to backend

--- Civic Track/Civic Track/Program.cs
+++ Civic Track/Civic Track/Program.cs
@@ -16,13 +16,10 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy.WithOrigins(
-                    .AllowAnyOrigin() // This allows all websites (Netlify, Vercel, Localhost)
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    //"http://127.0.0.1:5500", 
-                    //"http://localhost:5500", 
-                    //"https://online-complaint-management.netlify.app"
-                    //"https://online-complaint-and-investigation.vercel.app"
+                    "http://127.0.0.1:5500", 
+                    "http://localhost:5500", 
+                    "https://online-complaint-management.netlify.app"
+                    "https://online-complaint-and-investigation.vercel.app"
                   )
                   .AllowAnyHeader()
                   .AllowAnyMethod();
