74ce27f01076a440eb169317be1c59664e2fd586
Nduwayezu Sophonie <ntarerugamba1962@gmail.com>
2026-04-28 17:26:06 +0200

    fixed missing lines

--- Civic Track/Civic Track/Program.cs
+++ Civic Track/Civic Track/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddCors(options =>
             policy.WithOrigins(
                     "http://127.0.0.1:5500", 
                     "http://localhost:5500", 
+                    //solve the comman issue
                     "https://online-complaint-management.netlify.app",
                     "https://online-complaint-and-investigation.vercel.app"
                   )
